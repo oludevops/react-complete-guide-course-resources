038683e0e20171a76ee50111f88cf20765c04548
oludevops <oludevops@outlook.com>
2025-03-26 23:22:58 -0400

    03_started

--- attachments/03 React Essentials/App.jsx
+++ attachments/03 React Essentials/App.jsx
@@ -1,13 +1,12 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         Fundamental React concepts you will need for almost any app you are going to build!
@@ -16,12 +15,30 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+      <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block." image={componentsImg} />
+            <CoreConcept title="JSX" description="Syntax extension for JavaScript." image={componentsImg} />
+            <CoreConcept title="Props" description="Pass data between components." image={componentsImg} />
+            <CoreConcept title="State" description="Manage component data dynamically." image={componentsImg} />
+          </ul>
+        </section>
       </main>
     </div>
   );
